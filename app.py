255131e88dfe3c8855267f97aff73e665cfae4c7
Kavyanjali Karan <karankavyanjali77@gmail.com>
2026-03-01 20:09:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,27 +3,29 @@ import pandas as pd
 import joblib
 import os
 
+# Model Path Setup
 BASE_DIR = os.path.dirname(__file__)
 MODEL_DIR = os.path.join(BASE_DIR, "models")
 MODEL_PATH = os.path.join(MODEL_DIR, "kmeans.pkl")
 
+# Train model automatically if missing
 if not os.path.exists(MODEL_PATH):
     os.makedirs(MODEL_DIR, exist_ok=True)
     from src.train import pipeline
+    joblib.dump(pipeline, MODEL_PATH)
 
+# Load model
 model = joblib.load(MODEL_PATH)
 
+# UI
 st.title("Customer Segmentation Predictor")
 
-age = st.slider("Age",18,70)
-income = st.slider("Income",10000,100000)
-
-input_df = pd.DataFrame([[age,income]],columns=["Age","Income"])
+income = st.slider("Annual Income (k$)", 0, 150, 50)
+score = st.slider("Spending Score (1-100)", 1, 100, 50)
 
 if st.button("Predict Segment"):
 
     input_df = pd.DataFrame([[income, score]])
-
     input_df.columns = model.feature_names_in_
 
     pred = model.predict(input_df)
