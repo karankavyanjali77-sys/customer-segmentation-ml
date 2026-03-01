8043f3f4fbb7b5371781b9238423a4b8e9df9c53
Kavyanjali Karan <karankavyanjali77@gmail.com>
2026-03-01 21:29:32 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,25 +1,22 @@
 import pandas as pd
-import joblib
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
+import os
 
-# Load dataset
-data = pd.read_csv("data/data.csv")
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+DATA_PATH = os.path.join(BASE_DIR, "data", "data.csv")
 
-# Select features
 features = ['Annual Income (k$)', 'Spending Score (1-100)']
 
-# Creating pipeline
-pipeline = Pipeline([
-    ("scaler", StandardScaler()),
-    ("kmeans", KMeans(n_clusters=5, random_state=42))
-])
+def train_model():
+    data = pd.read_csv(DATA_PATH)
 
-# Train model
-pipeline.fit(data[features])
+    pipeline = Pipeline([
+        ("scaler", StandardScaler()),
+        ("kmeans", KMeans(n_clusters=5, random_state=42))
+    ])
 
-# Save model
-joblib.dump(pipeline, "models/kmeans.pkl")
+    pipeline.fit(data[features])
 
-print("Model trained and saved!")
+    return pipeline
